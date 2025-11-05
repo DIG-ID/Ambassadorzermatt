65367dcedea1f06c7c57a7555c1ed3170e2b239a
bankz91 <brunoduarte.91300@gmail.com>
2025-11-05 12:34:46 +0000

    1

--- template-parts/design-system.php
+++ template-parts/design-system.php
@@ -1,14 +1,31 @@
 <section class="design-system pb-16">
-    <div class="theme-container">
-        <h1 class="font-noto text-[58px]">Design System</h1>
-        <div class="theme-grid  col-span-2 md:col-span-2 xl:col-span-4 mt-12 border-l border-dark px-8">
-            <h2 class="#000 pb-16">Colors</h2>
-            <ul class="space-y-10">
-                <li class="w-[6.25rem] h-[6.25rem] rounded-full bg-Dark"></li>
-                <li class="w-[6.25rem] h-[6.25rem] rounded-full bg-Brown"></li>
-                <li class="w-[6.25rem] h-[6.25rem] rounded-full bg-LightGray"></li>
-                <li class="w-[6.25rem] h-[6.25rem] rounded-full bg-White"></li>
-            </ul>
-        <div>
-    </div>
+    <h1 class="title-main font-noto text-[58px]">Design System</h1>
+        <div class="theme-grid">
+            <div class="col-span-2 md:col-span-2 xl:col-span-4 mt-12 px-8 border-Dark border-r-2">
+                <h2 class="title-main">Colors</h2>
+                <ul class="space-y-10">
+                    <li class="w-[6.25rem] h-[6.25rem] rounded-full bg-Dark"></li>
+                    <li class="w-[6.25rem] h-[6.25rem] rounded-full bg-Brown"></li>
+                    <li class="w-[6.25rem] h-[6.25rem] rounded-full bg-LightGray"></li>
+                    <li class="w-[6.25rem] h-[6.25rem] rounded-full bg-White"></li>
+                </ul>
+            </div>
+            <div class="col-span-2 md:col-span-2 xl:col-span-4 mt-12 px-8 border-Dark border-r-2">
+                <h1 class="title-main font-inria">Titles</h1>
+                <p class="font-noto body ">Inria serif</p>
+                <h1 class="title-main">H1 Title</h1>
+                <h2 class="title-secondary">H2 Title</h2>
+            </div>
+            <div class="col-span-2 md:col-span-2 xl:col-span-4 mt-12 px-8 border-Dark border-r-2">
+                <h1 class="title-main font-inria">Paragraphs</h1>
+                <p class="font-noto body pb-8 ">Noto Sans</p>
+                <p class="font-noto body"> Text <br>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                </p>
+            </div>
+            <div class="col-span-2 md:col-span-2 xl:col-span-4 mt-12 px-8 border-Dark border-r-2">
+                <h1 class="title-main font-inria pb-8">Buttons</h1>
+                <a class="btn btn-primary">Mehr erfahren</a>
+            </div>
+        </div>
 </section>
