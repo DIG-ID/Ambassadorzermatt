e3dcea3a2c31820e9db424caf3188e1570cbc3fc
Daniel-Digid <daniel@dig.id>
2025-11-06 12:55:28 +0000

    d

--- template-parts/header-main.php
+++ template-parts/header-main.php
@@ -1,5 +1,41 @@
-<header id="header-main" class="header-main w-full z-50 overflow-hidden" itemscope itemtype="http://schema.org/WebSite">
-
-	
-
+<header id="header-main" class="header-main w-full relative z-50 overflow-visible" itemscope itemtype="http://schema.org/WebSite">
+  <div class="theme-container">
+    <div class="grid grid-cols-3">
+      <div class="col-span-1 flex items-center justify-start">
+        <nav id="primary-nav" class="primary-nav" aria-label="<?php esc_attr_e( 'Main Menu', 'ambassadorzermatt' ); ?>" role="navigation">
+          <?php
+          if ( has_nav_menu( 'main-menu' ) ) :
+            wp_nav_menu(
+							array(
+								'theme_location'  => 'main-menu',
+								'menu_id'         => 'main-menu',
+								'menu_class'      => 'main-menu-header flex flex-col lg:flex-row',
+								'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+								'fallback_cb'     => '__return_false',
+							)
+						);
+          else :
+            echo '<span>' . esc_html__( 'Please assign a menu to the Main Menu location', 'ambassadorzermatt' ) . '</span>';
+          endif;
+          ?>
+        </nav>
+      </div>
+      <div class="col-span-1 flex items-center justify-center">
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="header-logo absolute top-0 max-w-80 transition-all duration-500 ease-in-out" itemprop="url">
+          <?php do_action( 'theme_logo' ); ?>
+        </a>
+      </div>
+      <div class="col-span-1 flex items-center justify-end">
+        <div class="menu-toggle">
+          <button class="menu-toggle__button" aria-label="Menu Toggle" aria-controls="mega-menu-nav">
+            <span class="menu-toggle__bars">
+              <span class="menu-toggle__bar menu-toggle__bar--top"></span>
+              <span class="menu-toggle__bar menu-toggle__bar--middle"></span>
+              <span class="menu-toggle__bar menu-toggle__bar--bottom"></span>
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </header>
---
+++ template-parts/modules/section-hero.php
@@ -0,0 +1,38 @@
+<?php if ( 'full' === get_field( 'hero_section_type' ) ) : ?>
+	<section id="section-hero" class="section-hero relative h-[calc(100dvh-98px)] md:h-[calc(100dvh-98px)] w-full z-10">
+  <?php $bg_id  = get_field('hero_image'); ?>
+  <?php if ( $bg_id ) : ?>
+    <picture aria-hidden="true" class="pointer-events-none">
+      <source media="(max-width: 767px)" srcset="<?php echo esc_attr(wp_get_attachment_image_srcset( $bg_id, 'hero-mobile' ) ?: wp_get_attachment_image_url( $bg_id, 'hero-mobile' )); ?>" sizes="100vw" />
+			<?php echo wp_get_attachment_image(
+				$bg_id,
+				'full',
+				false,
+				[
+					'class' => 'w-full h-full object-cover',
+					'sizes' => '100vw',
+				]
+			); ?>
+    </picture>
+  <?php endif; ?>
+	
+	</section>
+<?php else : ?>
+	<section id="section-hero" class="section-hero relative w-full z-10">
+    <?php $bg_id  = get_field('hero_image'); ?>
+		<?php if ( $bg_id ) : ?>
+      <picture aria-hidden="true" class="pointer-events-none">
+        <source media="(max-width: 767px)" srcset="<?php echo esc_attr(wp_get_attachment_image_srcset( $bg_id, 'hero-mobile' ) ?: wp_get_attachment_image_url( $bg_id, 'hero-mobile' )); ?>" />
+        <?php echo wp_get_attachment_image(
+          $bg_id,
+          'full',
+          false,
+          [
+            'class' => 'w-full h-full object-cover',
+            'sizes' => 'full',
+          ]
+        ); ?>
+      </picture>
+    <?php endif; ?>
+	</section>
+<?php endif; ?>
