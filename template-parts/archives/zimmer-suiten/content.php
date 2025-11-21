afc5c57c28cbc6223fda174c3ac54cb57e8d21e6
Daniel-Digid <daniel@dig.id>
2025-11-21 13:50:46 +0000

    d

---
+++ template-parts/archives/zimmer-suiten/content.php
@@ -0,0 +1,99 @@
+<section id="section-content" class="section-content bg-White text-Dark pb-24 md:pb-28 xl:pb-36">
+  <div class="theme-container">
+    <?php
+    // Query zimmer-suiten posts
+    $zimmer_args  = array(
+      'post_type'      => 'zimmer-suiten',
+      'posts_per_page' => -1,
+      'post_status'    => 'publish',
+      'orderby'        => 'menu_order',
+      'order'          => 'ASC',
+    );
+
+    $zimmer_query = new WP_Query( $zimmer_args );
+
+    if ( $zimmer_query->have_posts() ) : ?>
+      <ul class="theme-grid xl:[&>li:nth-child(2n+2)]:[--offset:16.25rem] xl:[&>li:nth-child(2n+2)]:mt-[var(--offset)] xl:[&>li:nth-child(2n+2)]:mb-[calc(var(--offset)*-1)] xl:pb-[16.25rem]">
+        <?php
+        while ( $zimmer_query->have_posts() ) :
+          $zimmer_query->the_post();
+
+          // Get custom fields
+          $image_field   = get_field( 'hero_intro_image' );
+          $description   = get_field( 'hero_intro_text' );
+
+          // Support both "ID" and "Image array" return formats
+          $image_id = null;
+          if ( is_array( $image_field ) && isset( $image_field['ID'] ) ) {
+            $image_id = $image_field['ID'];
+          } elseif ( is_numeric( $image_field ) ) {
+            $image_id = (int) $image_field;
+          }
+
+          ?>
+          <li class="col-span-6 mb-16 md:mb-20 xl:mb-16">
+            <article id="experience-<?php the_ID(); ?>" <?php post_class( 'card card__experience' ); ?>>
+              <header class="card-header mb-5 md:mb-8">
+                <?php
+                if ( $image_id ) :
+                  // You can also use wp_get_attachment_image() if you prefer
+                  echo '<a href="' . esc_url( get_permalink() ) . '">';
+                  echo wp_get_attachment_image(
+                    $image_id,
+                    'full',
+                    false,
+                    array(
+                      'class' => 'mb-8 max-h-[825px] w-full object-cover xl:object-[50%_50%] xl:min-h-[825px] xl:max-h-[825px]',
+                    )
+                  );
+                  echo '</a>';
+                endif;
+                ?>
+                <a href="<?php the_permalink(); ?>">
+                  <h3 class="title-main"><?php the_title(); ?></h3>
+                </a>
+              </header><!-- .entry-header -->
+
+              <div class="card-content md:grid md:grid-cols-6">
+                <div class="md:col-span-6 xl:col-span-5 mb-10 md:mb-14 xl:mb-12">
+                  <div class="overview-description mb-7">
+                    <?php
+                    if ( $description ) {
+                      echo wp_kses_post( $description );
+                    } else {
+                      the_excerpt();
+                    }
+                    ?>
+                  </div>
+                  <div class="overview-features">
+                    <div class="flex flex-row items-center gap-7 mb-4">
+                      <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svg/features_size.svg" alt="Size" title="Size" class="w-8" />
+                      <p><?php esc_html_e( 'Grösse: ','ambassador' ); ?><?php the_field( 'overview_size' ); ?></p>
+                    </div>
+                    <div class="flex flex-row items-center gap-7 mb-4">
+                      <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svg/features_bed.svg" alt="Size" title="Size" class="w-8" />
+                      <p><?php esc_html_e( 'Bett: ','ambassador' ); ?><?php the_field( 'overview_bed' ); ?></p>
+                    </div>
+                    <div class="flex flex-row items-center gap-7 mb-4">
+                      <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/svg/features_occupancy.svg" alt="Size" title="Size" class="w-8" />
+                      <p><?php esc_html_e( 'Belegung: ','ambassador' ); ?><?php the_field( 'overview_occupancy' ); ?></p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="md:col-span-4 xl:col-span-5">
+                  <a href="<?php the_permalink(); ?>" class="btn btn-primary">
+                    <?php esc_html_e( 'Mehr erfahren', 'ambassador' ); ?>
+                  </a>
+                </div>
+              </div>
+            </article>
+          </li>
+        <?php endwhile; ?>
+      </ul>
+      <?php
+      wp_reset_postdata();
+    endif;
+    ?>
+  </div>
+</section>
