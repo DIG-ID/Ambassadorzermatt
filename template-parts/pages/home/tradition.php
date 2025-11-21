ad0d6fb274712f7cb87074a435efa0eb023e64d1
Daniel-Digid <daniel@dig.id>
2025-11-21 16:30:55 +0000

    Update tradition.php

--- template-parts/pages/home/tradition.php
+++ template-parts/pages/home/tradition.php
@@ -1,18 +1,17 @@
 <section id="section-tradition" class="section-tradition bg-LightGray relative h-[90dvh] md:h-screen w-full z-20 flex flex-col justify-end md:justify-center">
-  <picture>
-    
-    <?php echo wp_get_attachment_image(
-      get_field('tradition_image'),
+  <?php
+  $bg_id = get_field( 'tradition_image' );
+  if ( $bg_id ) :
+    echo wp_get_attachment_image(
+      $bg_id,
       'full',
       false,
       [
-        'class'          => 'absolute inset-0 w-full h-1/2 md:h-full object-cover -z-10',
-        'sizes'          => '100vw',
-        'fetchpriority'  => 'high',
-        'decoding'       => 'async',
+        'class' => 'absolute inset-0 w-full h-1/2 md:h-full object-cover -z-10 object-center min-h-[540px] md:min-h-[440px]',
       ]
-    ); ?>
-  </picture>
+    );
+  endif;
+  ?>
   <div class="bg-[linear-gradient(#F3F3F3,#F3F3F3)] md:bg-[length:70%_100%] xl:bg-[length:50%_100%] bg-right bg-no-repeat pt-8 pb-16 md:py-16 xl:pt-16 xl:pb-12">
     <div class="theme-container">
       <div class="theme-grid">
