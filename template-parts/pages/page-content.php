8e5aad4b560a0b7060dcc524a168ec892f1110bf
Daniel-Digid <daniel@dig.id>
2025-11-25 11:06:00 +0000

    d

---
+++ template-parts/pages/page-content.php
@@ -0,0 +1,14 @@
+<section id="section-page-content" class="section-page-content bg-White pt-14 pb-24 md:py-24 xl:py-36">
+	<div class="theme-container">
+		<div class="theme-grid">
+      <div class="col-span-2 md:col-span-6 xl:col-span-10">
+        <h1 class="font-inria text-[3.125rem] md:text-[5rem] xl:text-[5.625rem] font-normal xl:font-light italic xl:not-italic leading-[2.5rem] md:leading-[5.9375rem] xl:leading-[6.25rem] tracking-[0.03125rem] mb-14 md:mb-24"><?php the_title(); ?></h1>
+      </div>
+			<div class="col-span-2 md:col-span-6 xl:col-span-10">
+				<div class="default-content-wrapper">
+					<?php the_content(); ?>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
---
+++ template-parts/pages/page-header.php
@@ -0,0 +1,3 @@
+<section id="section-hero" class="section-hero relative h-64 w-full bg-Dark">
+
+</section>
