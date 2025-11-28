659477d6f9492c4819ab1c332dbee0cfa05b6ae0
bankz91 <brunoduarte.91300@gmail.com>
2025-11-28 11:53:05 +0000

    1

--- template-parts/pages/arrival-contact/form.php
+++ template-parts/pages/arrival-contact/form.php
@@ -1,9 +1,8 @@
 <section id="section-form" class="section-form bg-White pt-[6.25rem] md:pt-[6.53rem] xl:pt-[9.38rem] pb-[6.25rem] xl:pb-[6.25rem]">
     <div class="theme-container">
         <div class="theme-grid bg-Dark">
-            <div class="col-start-1 col-span-2 md:col-start-2 md:col-span-4 xl:col-start-2 xl:col-span-10 text-White xl:py-[3.19rem]">
-                <?php echo do_shortcode('[contact-form-7 id="6f1e299" title="Contact form 1"]'); ?>
-
+            <div class="col-start-1 col-span-2 md:col-start-2 md:col-span-4 xl:col-start-2 xl:col-span-10 text-White py-[3.69rem] md:py-[3.8rem] xl:py-[3.19rem]">
+                <?php echo do_shortcode(the_field('contact_form_short_code')); ?>
             </div>
         </div>
     </div>
