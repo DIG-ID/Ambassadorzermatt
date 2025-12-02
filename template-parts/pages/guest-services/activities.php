2df14714bd545ace6f19f9cb0e81ebcc5e04538e
Daniel-Digid <daniel@dig.id>
2025-12-02 14:56:59 +0000

    d

--- template-parts/pages/guest-services/activities.php
+++ template-parts/pages/guest-services/activities.php
@@ -31,7 +31,7 @@
             <div class="grid grid-cols-8 md:grid-cols-5 xl:grid-cols-8 gap-5">
 
               <!-- Title column -->
-              <div class="col-span-7 md:col-span-2">
+              <div class="col-span-7 md:col-span-2 xl:col-span-3">
                 <?php if ( $title ) : ?>
                   <h2 class="text-Dark title-secondary">
                     <?php echo esc_html( $title ); ?>
