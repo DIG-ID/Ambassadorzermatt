485e1eab21568a305a9a9e6d55eba426c80b9eb7
Pedro <pedro@dig.id>
2025-12-05 15:01:51 +0000

    restaurant headings

--- template-parts/pages/restaurant-carbon/chefs.php
+++ template-parts/pages/restaurant-carbon/chefs.php
@@ -1,68 +1,79 @@
+<?php
+/**
+ * Chefs Section in the Restaurant Carbon Page Template.
+ *
+ * @package ambassador-zermatt
+ * @subpackage Section
+ * @since 1.0.0
+ */
+
+?>
+
 <section id="chefs" class="chefs bg-Dark xl:pt-36"> 
-    <div class="theme-container">
-        <div class="theme-grid pb-[6.2rem] xl:pb-[9.37rem]">
-            <div class="col-span-2 xl:col-span-6 pt-[3.74rem] md:pt-0 order-2 md:order-none">
-                <?php
-                    $imgLogo = get_field('chefs_chef_image');
-                    $size  = 'full';
+	<div class="theme-container">
+		<div class="theme-grid pb-[6.2rem] xl:pb-[9.37rem]">
+			<div class="col-span-2 xl:col-span-6 pt-[3.74rem] md:pt-0 order-2 md:order-none">
+				<?php
+					$imgLogo = get_field('chefs_chef_image');
+					$size  = 'full';
 
-                    if ( $imgLogo ) {
-                    echo wp_get_attachment_image(
-                    $imgLogo,
-                    $size,
-                    false,
-                    [
-                    'class'    => 'w-full max-h-auto bleed-left-full-md',
-                    'loading'  => 'eager',
-                    'decoding' => 'async',
-                    ]
-                    );
-                    }
-                ?>
-            </div>
-            <div class="col-span-2 md:col-start-3 md:col-span-4 xl:col-start-7 xl:col-span-5 order-1 md:order-none">
-                <div class="title-secondary text-LightGray">
-                    <h2><?php the_field('chefs_over_title')?></h2>
-                </div>
-                <div class="title-main text-LightGray pt-[1.88rem] md:pt-[1.36rem] xl:pt-[1.87rem]">
-                    <h1><?php the_field('chefs_chef_name')?></h1>
-                </div>
-                <div class="body text-LightGray pt-[1.87rem]">
-                    <p><?php the_field('chefs_chef_description')?></p>
-                </div>
-            </div>
-        </div>
-        <div class="theme-grid pb-[6.2rem] xl:pb-[9.37rem] md:pt-0">
-            <div class="col-span-2 md:col-span-4 xl:col-span-5">
-                <div class="title-secondary text-LightGray">
-                    <h2><?php the_field('chefs_over_title_2')?></h2>
-                </div>
-                <div class="title-main text-LightGray pt-[1.88rem] md:pt-[1.36rem] xl:pt-[1.87rem]">
-                    <h1><?php the_field('chefs_chef_name_2')?></h1>
-                </div>
-                <div class="body text-LightGray pt-[1.88rem] md:pt-[1.88rem]">
-                    <p><?php the_field('chefs_chef_description_2')?></p>
-                </div>
-            </div>
-            <div class="col-span-2 md:col-span-2 md:col-start-5 xl:col-start-7 xl:col-span-6 pt-[3.77rem] md:pt-0">
-                <?php
-                    $imgLogo = get_field('chefs_chef_image_2');
-                    $size  = 'full';
+					if ( $imgLogo ) {
+					echo wp_get_attachment_image(
+					$imgLogo,
+					$size,
+					false,
+					[
+					'class'    => 'w-full max-h-auto bleed-left-full-md',
+					'loading'  => 'eager',
+					'decoding' => 'async',
+					]
+					);
+					}
+				?>
+			</div>
+			<div class="col-span-2 md:col-start-3 md:col-span-4 xl:col-start-7 xl:col-span-5 order-1 md:order-none">
+				<div class="title-secondary text-LightGray">
+					<h2><?php the_field('chefs_over_title')?></h2>
+				</div>
+				<div class="title-main text-LightGray pt-[1.88rem] md:pt-[1.36rem] xl:pt-[1.87rem]">
+					<h3><?php the_field('chefs_chef_name')?></h3>
+				</div>
+				<div class="body text-LightGray pt-[1.87rem]">
+					<p><?php the_field('chefs_chef_description')?></p>
+				</div>
+			</div>
+		</div>
+		<div class="theme-grid pb-[6.2rem] xl:pb-[9.37rem] md:pt-0">
+			<div class="col-span-2 md:col-span-4 xl:col-span-5">
+				<div class="title-secondary text-LightGray">
+					<h2><?php the_field('chefs_over_title_2')?></h2>
+				</div>
+				<div class="title-main text-LightGray pt-[1.88rem] md:pt-[1.36rem] xl:pt-[1.87rem]">
+					<h3><?php the_field('chefs_chef_name_2')?></h3>
+				</div>
+				<div class="body text-LightGray pt-[1.88rem] md:pt-[1.88rem]">
+					<p><?php the_field('chefs_chef_description_2')?></p>
+				</div>
+			</div>
+			<div class="col-span-2 md:col-span-2 md:col-start-5 xl:col-start-7 xl:col-span-6 pt-[3.77rem] md:pt-0">
+				<?php
+					$imgLogo = get_field('chefs_chef_image_2');
+					$size  = 'full';
 
-                    if ( $imgLogo ) {
-                    echo wp_get_attachment_image(
-                    $imgLogo,
-                    $size,
-                    false,
-                    [
-                    'class'    => 'w-full max-h-auto object-cover xl:max-h-[758px] bleed-right-full-md',
-                    'loading'  => 'eager',
-                    'decoding' => 'async',
-                    ]
-                    );
-                    }
-                ?>
-            </div>
-        </div>
-    </div>
+					if ( $imgLogo ) {
+					echo wp_get_attachment_image(
+					$imgLogo,
+					$size,
+					false,
+					[
+					'class'    => 'w-full max-h-auto object-cover xl:max-h-[758px] bleed-right-full-md',
+					'loading'  => 'eager',
+					'decoding' => 'async',
+					]
+					);
+					}
+				?>
+			</div>
+		</div>
+	</div>
 </section>
--- template-parts/pages/restaurant-carbon/intro.php
+++ template-parts/pages/restaurant-carbon/intro.php
@@ -1,16 +1,27 @@
+<?php
+/**
+ * Intro Section in the Restaurant Carbon Page Template.
+ *
+ * @package ambassador-zermatt
+ * @subpackage Section
+ * @since 1.0.0
+ */
+
+?>
+
 <section id="intro-restaurant-carbon" class="intro-restaurant-carbon bg-Dark pt-[2.5rem] pb-[6.25rem] md:pt-[6.25rem] xl:pb-0">
-    <div class="theme-container">
-        <div class="theme-grid">
-            <div class="col-start-1 col-span-2 md:col-start-1 md:col-span-4 xl:col-start-2 xl:col-span-4">
-                <div class="title-big text-LightGray xl:max-w-[30rem]">
-                    <h1><?php the_field( 'intro_title' ); ?></h1>
-                </div>
-            </div>
-            <div class="col-start-1 col-span-2 md:col-start-1 md:col-span-4 xl:col-start-8 xl:col-span-4">
-                <div class="body text-LightGray pt-[1.25rem] xl:pt-0">
-                    <p><?php the_field( 'intro_text' ); ?></p>
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="theme-container">
+		<div class="theme-grid">
+			<div class="col-start-1 col-span-2 md:col-start-1 md:col-span-4 xl:col-start-2 xl:col-span-4">
+				<div class="title-big text-LightGray xl:max-w-[30rem]">
+					<h1><?php the_field( 'intro_title' ); ?></h1>
+				</div>
+			</div>
+			<div class="col-start-1 col-span-2 md:col-start-1 md:col-span-4 xl:col-start-8 xl:col-span-4">
+				<div class="body text-LightGray pt-[1.25rem] xl:pt-0">
+					<p><?php the_field( 'intro_text' ); ?></p>
+				</div>
+			</div>
+		</div>
+	</div>
 </section>
