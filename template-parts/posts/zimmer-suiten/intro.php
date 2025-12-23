67c16462f4cd20263bf66ec552173b9a42ad95e5
Daniel-Digid <daniel@dig.id>
2025-12-23 14:41:38 +0000

    d

--- template-parts/posts/zimmer-suiten/intro.php
+++ template-parts/posts/zimmer-suiten/intro.php
@@ -9,9 +9,19 @@
 
 ?>
 
-<section id="section-intro" class="section-intro bg-White pt-10 pb-14 md:pt-14 md:pb-24 xl:pt-24 xl:pb-20">
+<section id="section-intro" class="section-intro bg-White pt-8 pb-14 md:pt-8 md:pb-24 xl:pt-10 xl:pb-20">
 	<div class="theme-container">
 		<div class="theme-grid">
+			<div class="col-start-1 col-span-2 md:col-span-6 xl:col-span-12 mb-7 md:mb-8 xl:mb-10">
+				<a href="<?php the_field( 'back_to_overview_link','option' ); ?>" class="back_to_overview-wrapper flex flex-row items-center">
+					<figure>
+						<svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 23 23" fill="none">
+							<path d="M13.1055 22.0352L1.41297 11.3511M1.41297 11.3511L11.168 0.675323M1.41297 11.3511L22.8605 11.3594" stroke="#766A66" stroke-width="2"/>
+						</svg>
+					</figure>
+					<span class="ml-3 text-Brown"><?php the_field( 'back_to_overview_label','option' ); ?></span>
+				</a>
+			</div>
 			<div class="col-start-1 col-span-2 md:col-start-1 md:col-span-4 xl:col-start-2 xl:col-span-4">
 				<div class="title-secondary text-Brown">
 					<h2><?php the_field( 'hero_intro_over_title' ); ?></h2>
