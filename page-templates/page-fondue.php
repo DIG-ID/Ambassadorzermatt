edbcdcafcbe6869ecf88fe523890e9ff3613bf80
bankz91 <brunoduarte.91300@gmail.com>
2026-02-18 16:42:23 +0000

    1

---
+++ page-templates/page-fondue.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *
+ * Template Name: Fondue Template
+ *
+ * @package ambassador-zermatt
+ * @subpackage Template
+ * @since 1.0.0
+ */
+
+get_header();
+if ( have_posts() ) :
+	while ( have_posts() ) :
+		the_post();
+		do_action( 'before_main_content' );
+			get_template_part( 'template-parts/modules/section', 'hero' );
+			get_template_part( 'template-parts/modules/section', 'intro' );
+			get_template_part( 'template-parts/pages/fondue/content' );
+			get_template_part( 'template-parts/modules/section', 'outro' );
+		do_action( 'after_main_content' );
+	endwhile;
+endif;
+get_footer();
