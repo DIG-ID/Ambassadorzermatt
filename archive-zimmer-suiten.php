238e04d5040596370a18f2645f61de7793aa2719
Pedro <pedro@dig.id>
2026-07-07 17:35:53 +0100

    up

--- archive-zimmer-suiten.php
+++ archive-zimmer-suiten.php
@@ -12,6 +12,7 @@ get_header();
 		get_template_part( 'template-parts/archives/zimmer-suiten/hero' );
 		get_template_part( 'template-parts/archives/zimmer-suiten/intro' );
 		get_template_part( 'template-parts/archives/zimmer-suiten/content' );
+		get_template_part( 'template-parts/archives/zimmer-suiten/faq' );
 		get_template_part( 'template-parts/archives/zimmer-suiten/outro' );
 	do_action( 'after_main_content' );
 get_footer();
