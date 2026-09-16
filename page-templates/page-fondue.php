48e44b745dbdfb8ae7f4683b3f4a2debbf95eecc
Pedro <pedro@dig.id>
2026-09-16 17:01:14 +0100

    up

--- page-templates/page-fondue.php
+++ page-templates/page-fondue.php
@@ -17,6 +17,7 @@ if ( have_posts() ) :
 			get_template_part( 'template-parts/pages/fondue/intro' );
 			get_template_part( 'template-parts/pages/fondue/content' );
 			get_template_part( 'template-parts/modules/section', 'outro' );
+			get_template_part( 'template-parts/modules/section', 'faq' );
 		do_action( 'after_main_content' );
 	endwhile;
 endif;
